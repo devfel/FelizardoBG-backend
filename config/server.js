60de1916e5d20708bbd6aa24160de0f9ec37f427
devfel <devfelizardo@gmail.com>
2024-04-05 23:58:39 -0300

    changing adptable configuration

--- config/server.js
+++ config/server.js
@@ -1,32 +1,29 @@
 // Path: ./config/server.js
 
-// Local Configuration
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", true),
-  },
-});
-
+// // Local Configuration
 // module.exports = ({ env }) => ({
-//   proxy: true,
-//   host: "0.0.0.0",
-//   port: process.env.PORT,
-//   url: env("APP_URL"),
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", 1337),
 //   app: {
 //     keys: env.array("APP_KEYS"),
 //   },
-//   admin: {
-//     path: "/admin",
-//     build: {
-//       backend: env("APP_URL", "https://tabuai-backend.adaptable.app"),
-//     },
-//     auth: {
-//       secret: env("ADMIN_JWT_SECRET"),
-//     },
+//   webhooks: {
+//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", true),
 //   },
 // });
+
+// Production Configuration
+module.exports = ({ env }) => ({
+  proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  url: env("EXTERNAL_URL"),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
+});
