2bc137f0586aa188c35101d669bf912e75e2a032
devfel <devfelizardo@gmail.com>
2024-04-05 21:36:26 -0300

    Changing the config server file

--- config/server.js
+++ config/server.js
@@ -1,10 +1,28 @@
+// Path: ./config/server.js
+
+// // Local Configuration
+// module.exports = ({ env }) => ({
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", 1337),
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", true),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  url: env("APP_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", true),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
   },
 });
