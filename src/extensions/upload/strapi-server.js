50eed3065b781d258c65d6a37d09e17e9d2436ee
devfel <devfelizardo@gmail.com>
2024-04-11 21:43:45 -0300

    adding file to bypass image manipulation and memory leak

---
+++ src/extensions/upload/strapi-server.js
@@ -0,0 +1,15 @@
+module.exports = (plugin) => {
+  const imageServices = plugin.services["image-manipulation"]();
+  plugin.services["image-manipulation"] = () => ({
+    ...imageServices,
+    generateThumbnail: () => null,
+    optimize: () => null,
+    isSupportedImage: () => null,
+    getDimensions: () => null,
+    generateResponsiveFormats: () => null,
+    isOptimizableImage: () => false,
+    isImage: () => false,
+  });
+
+  return plugin;
+};
